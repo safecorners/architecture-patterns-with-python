c5b88ecf6266519d7bb49e8a789db3d2dbbe7c22
safecorners <safeconers.mobile@gmail.com>
2023-02-10 02:09:45 +0900

    add repository

---
+++ tests/integration/test_orm.py
@@ -0,0 +1,29 @@
+from sqlalchemy.sql import text
+
+from domain.model import OrderLine
+
+
+def test_orderline_mapper_can_load_lines(session):
+    session.execute(
+        text(
+            "INSERT INTO order_lines (orderid, sku, qty) VALUES "
+            + '("order1", "RED-CHAIR", 12),'
+            + '("order1", "RED-TABLE", 13),'
+            + '("order2", "BLUE-LIPSTICK", 14)'
+        )
+    )
+    expected = [
+        OrderLine("order1", "RED-CHAIR", 12),
+        OrderLine("order1", "RED-TABLE", 13),
+        OrderLine("order2", "BLUE-LIPSTICK", 14),
+    ]
+    assert session.query(OrderLine).all() == expected
+
+
+def test_orderline_mapper_can_save_lines(session):
+    new_line = OrderLine("order1", "DECORATIVE-WIDGET", 12)
+    session.add(new_line)
+    session.commit()
+
+    rows = list(session.execute(text('SELECT orderid, sku, qty FROM "order_lines"')))
+    assert rows == [("order1", "DECORATIVE-WIDGET", 12)]
