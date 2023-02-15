2b5563c9ee421a2d681b69434b344b9f6f75af1a
safecorners <safeconers.mobile@gmail.com>
2023-02-15 12:42:54 +0900

    change env-var to success e2e-tests in docker

--- src/allocation/service/services.py
+++ src/allocation/service/services.py
@@ -1,6 +1,10 @@
+from __future__ import annotations
+
+from datetime import date
+from typing import Optional
+
 from allocation.adapters.repository import AbstractRepository
 from allocation.domain import model
-from allocation.domain.model import OrderLine
 
 
 class InvalidSku(Exception):
@@ -11,10 +15,20 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+def allocate(
+    orderid: str, sku: str, qty: int, repo: AbstractRepository, session
+) -> str:
+    line = model.OrderLine(orderid, sku, qty)
     batches = repo.list()
     if not is_valid_sku(line.sku, batches):
         raise InvalidSku(f"Invalid sku {line.sku}")
     batchref = model.allocate(line, batches)
     session.commit()
     return batchref
+
+
+def add_batch(
+    ref: str, sku: str, qty: int, eta: Optional[date], repo: AbstractRepository, session
+):
+    repo.add(model.Batch(ref, sku, qty, eta))
+    session.commit()
--- tests/unit/test_services.py
+++ tests/unit/test_services.py
@@ -12,29 +12,33 @@ class FakeSession:
         self.committed = True
 
 
-def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATE-LAMP", 10)
-    batch = model.Batch("b1", "COMPLICATE-LAMP", 100, eta=None)
-    repo = repository.InMemoryRepository([batch])
+def test_add_batch():
+    repo, session = repository.InMemoryRepository([]), FakeSession()
+    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, repo, session)
 
-    result = services.allocate(line, repo, FakeSession())
+    assert repo.get("b1") is not None
+    assert session.committed
+
+
+def test_returns_allocation():
+    repo, session = repository.InMemoryRepository([]), FakeSession()
+    services.add_batch("b1", "COMPLICATE-LAMP", 100, None, repo, session)
+    result = services.allocate("o1", "COMPLICATE-LAMP", 10, repo, session)
     assert result == "b1"
 
 
 def test_erorr_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
-    batch = model.Batch("b1", "AREAL-SKU", 100, eta=None)
-    repo = repository.InMemoryRepository([batch])
+    repo, session = repository.InMemoryRepository([]), FakeSession()
+    services.add_batch("b1", "AREAL-SKU", 100, None, repo, session)
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate(line, repo, FakeSession())
+        services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
 
 
 def test_commits():
-    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
     batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
     repo = repository.InMemoryRepository([batch])
     session = FakeSession()
 
-    services.allocate(line, repo, session)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, repo, session)
     assert session.committed is True
