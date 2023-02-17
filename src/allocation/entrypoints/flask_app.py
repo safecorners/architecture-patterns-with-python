8a316646d93c66cc8dcd47f4e7c624cf5425d11c
safecorners <safeconers.mobile@gmail.com>
2023-02-18 02:58:55 +0900

    change api endpoints

--- src/allocation/entrypoints/flask_app.py
+++ src/allocation/entrypoints/flask_app.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from flask import Flask, jsonify, request
 
 from allocation.adapters import orm
-from allocation.service import handlers, unit_of_work
+from allocation.domain import events
+from allocation.service import handlers, messagebus, unit_of_work
 
 orm.start_mappers()
 app = Flask(__name__)
@@ -12,13 +13,13 @@ app = Flask(__name__)
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
     try:
-        # need validator for type narrowing
-        batchref = handlers.allocate(
+        event = events.AllocationRequired(
             request.json["orderid"],
             request.json["sku"],
             request.json["qty"],
-            unit_of_work.SqlAlchemyUnitOfWork(),
         )
+        results = messagebus.handle(event, unit_of_work.SqlAlchemyUnitOfWork())
+        batchref = results.pop(0)
     except handlers.InvalidSku as e:
         return jsonify({"message": str(e)}), 400
 
@@ -30,11 +31,11 @@ def add_batch():
     eta = request.json["eta"]
     if eta is not None:
         eta = datetime.fromisoformat(eta).date()
-    handlers.add_batch(
+    event = events.BatchCreated(
         request.json["ref"],
         request.json["sku"],
         request.json["qty"],
         eta,
-        unit_of_work.SqlAlchemyUnitOfWork(),
     )
+    messagebus.handle(event, unit_of_work.SqlAlchemyUnitOfWork())
     return "OK", 201
